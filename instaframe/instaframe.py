817f3fcee08836bb0d84cfae12a986d26912c6ff
Pierfrancesco Oselin <pierfrancescooselin@gmail.com>
2022-01-28 08:41:21 +0100

    Completing images merge

--- instaframe/instaframe.py
+++ instaframe/instaframe.py
@@ -6,13 +6,13 @@ class Instaframe:
         self.width  = width
         self.height = height
         self.border = border
-        self.frame  = np.zeros([self.width, self.height, 3], dtype=np.uint8)
+        self.frame  = np.zeros([self.height, self.width, 3], dtype=np.uint8)
         self.frame.fill(255)
         self.img    = []
 
     #Private methods
     def __update_frame(self):
-        self.frame = np.zeros([self.width, self.height, 3], dtype=np.uint8)
+        self.frame = np.zeros([self.height, self.width, 3], dtype=np.uint8)
         self.frame.fill(255)
 
     def __resize(self, index):
@@ -21,7 +21,6 @@ class Instaframe:
         new_height = int(self.img[index].shape[0] * scale_percent)
 
         self.img[index] =  cv2.resize(self.img[index], [new_width, new_height], interpolation = cv2.INTER_AREA)
-        print(self.img[index].shape)
 
     #Public methods
     def set_frame(self,width, height):
@@ -42,13 +41,11 @@ class Instaframe:
     
     def set_img(self, img):
         #check if img is an image
-        print(img.shape)
         self.img.append(img)
     
     def set_image_frompath(self, path):
         #check if the image actually exists
         self.img.append(cv2.imread(path, cv2.IMREAD_UNCHANGED))
-        print(self.img[-1].shape)
     
     def show(self, index = 0):
         if len(self.img) > 0:
@@ -57,7 +54,7 @@ class Instaframe:
         cv2.imshow("Photo",self.frame)
 
     def get_dim(self):
-        print(self.width,self.height)
+        print(self.height,self.width)
 
     def save(self, mode):
         self.__resize(0)
@@ -70,48 +67,58 @@ class Instaframe:
 
     def compose(self,mode = ""):
         img = np.zeros([self.frame.shape[0], self.frame.shape[1],3], dtype=np.uint8)
-        lim = [int((self.frame.shape[0]-self.img[0].shape[0])/2), int((self.frame.shape[1]-self.img[1].shape[0])/2)]
+        lim = [int((self.frame.shape[0]-self.img[0].shape[0])/2), int((self.frame.shape[1]-self.img[0].shape[1])/2)]
+        
         #Image composition
-        for layer in range(3):
-            for row in range(self.frame.shape[1]):
-                for column in range(self.frame.shape[0]):
-                    #print (row,column)
-                    #print("CHECK:",self.frame.shape[0]-2*self.border)
-                    if (row < lim[1] or row > (self.frame.shape[1]-lim[1])) or (column < lim[0] or column > (self.frame.shape[0]-lim[0])):
-                        img[column,row,layer] = self.frame[column,row,layer]
-                    else:
-                        if mode == "":
-                            img[column, row, layer] = self.frame[column, row, layer]
+        threshold = 0
+        for row in range(self.frame.shape[0]):
+            for column in range(self.frame.shape[1]):
+                if row < lim[0] or row >= (self.frame.shape[0]-lim[0]) or column < lim[1] or column >= (self.frame.shape[1]-lim[1]):
+                    img[row,column,:] = self.frame[row,column,:]
+                else:
+                    irow    = row   -lim[0]
+                    icolumn = column-lim[1]
 
-                        elif mode == '|':
-                            #print(row,column)
-                            if (column < self.frame.shape[0]/2):
-                                img[column, row, layer] = self.img[0][column-lim[0], row-lim[1], layer]
-                            else:
-                                img[column, row, layer] = self.img[1][column-lim[0], row-lim[1], layer]
+                    if mode == "":
+                        img[row, column, :] = self.frame[row, column, :]
 
-                        elif mode == '/':
-                            if (row >= column):
-                                img[row,column,layer] = self.img[0][row-self.border,column-self.border,layer]
-                            else:
-                                img[row,column,layer] = self.img[1][row-self.border,column-self.border,layer]
-
-                        elif mode == '-':
-                            if (row < self.frame.shape[1]/2):
-                                img[row,column,layer] = self.img[0][row-self.border,column-self.border,layer]
-                            else:
-                                img[row,column,layer] = self.img[1][row-self.border,column-self.border,layer]
-
-                        elif mode == '\\':
-                            if (row <= column ):
-                                img[row,column,layer] = self.img[0][row-self.border,column-self.border,layer]
-                            else:
-                                img[row,column,layer] = self.img[1][row-self.border,column-self.border,layer]
-
+                    elif mode == '|':
+                        if (column < self.frame.shape[1]/2):
+                            img[row, column, :] = self.img[0][irow, icolumn, :]
                         else:
-                            raise ""
+                            img[row, column, :] = self.img[1][irow, icolumn, :]
 
+                    elif mode == '/':       
+                        if (icolumn == 0):
+                            threshold -= 1
+                        if (irow == 0 and icolumn == 0):
+                            threshold = (min(self.img[0].shape[0],self.img[0].shape[1])+self.frame.shape[1]-2*lim[1])/2   
 
+                        if (icolumn < threshold):
+                            img[row, column, :] = self.img[0][irow, icolumn, :]
+                        else:
+                            img[row, column, :] = self.img[1][irow, icolumn, :]
+                        
+
+                    elif mode == '-':
+                        if (row < self.frame.shape[0]/2):
+                            img[row, column, :] = self.img[0][irow, icolumn, :]
+                        else:
+                            img[row, column, :] = self.img[1][irow, icolumn, :]
+
+                    elif mode == '\\':
+                        if (icolumn == 0):
+                            threshold += 1
+                        if (irow == 0 and icolumn == 0):
+                            threshold = (-min(self.img[0].shape[0],self.img[0].shape[1])+self.frame.shape[1])/2
+
+                        if (column < threshold):
+                            img[row, column, :] = self.img[0][irow, icolumn, :]
+                        else:
+                            img[row, column, :] = self.img[1][irow, icolumn, :]
+
+                    else:
+                        raise ""
+                    
         return img
     
-
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time as t
 import numpy as np
 import cv2
 import sys
-myphoto = Instaframe(1080,1080)
+myphoto = Instaframe(1080,1350)
 
 myphoto.set_border(100)
 
@@ -12,9 +12,9 @@ myphoto.set_border(100)
 #myphoto.resize()
 
 #myphoto.show_frame()
-myphoto.set_image_frompath("pic.jpg")
-myphoto.set_image_frompath("pic2.jpg")
+myphoto.set_image_frompath("media/pic.jpg")
+myphoto.set_image_frompath("media/pic2.jpg")
 
 myphoto.get_dim()
-cv2.imwrite('test.jpg', myphoto.save("|"))
+cv2.imwrite('media/out.jpg', myphoto.save("/"))
 
